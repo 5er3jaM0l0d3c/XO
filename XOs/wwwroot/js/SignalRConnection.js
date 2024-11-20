70f5143db5e716f76e530b947d6c174440829837
5er3jaM0l0d3c <telephonecizerwed@gmail.com>
2024-11-21 01:04:43 +0300

    исправил баги

--- XOs/wwwroot/js/SignalRConnection.js
+++ XOs/wwwroot/js/SignalRConnection.js
@@ -1,9 +1,7 @@
 ﻿let isConnected = false;
 // Создаем подключение к Hub
 const connection = new signalR.HubConnectionBuilder()
-    .withUrl("/chatHub", {
-        timeout: 60000
-    })
+    .withUrl("/chatHub")
     .build();
 connection.serverTimeoutInMilliseconds = 600000; // 600 seconds
 connection.keepAliveIntervalInMilliseconds = 120000;
@@ -14,9 +12,6 @@ connection.start().then(() => {
 
 
     connection.invoke("SendLog", sessionStorage.getItem("name") + " : подключился(" + document.location.href + ")").catch(err => {
-        console.error(err.toString());
-    });
-    connection.invoke("CheckWaiting").catch(err => {
         console.error(err.toString());
     });
     
